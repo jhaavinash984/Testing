2a67f55ea1c3d53b75d61a9fcc167e7275de88f4
avijha2 <avijha2@cisco.com>
2023-12-05 15:21:53 +0530

    change for deployment

--- src/main/java/com/test/jenkin/SpringbootJenkinApplication.java
+++ src/main/java/com/test/jenkin/SpringbootJenkinApplication.java
@@ -2,9 +2,16 @@ package com.test.jenkin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringbootJenkinApplication {
+public class SpringbootJenkinApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringbootJenkinApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootJenkinApplication.class, args);
